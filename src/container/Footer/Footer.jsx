838961b660923644824e056f7f8f870e93f8583e
dmarcela2022 <104063341+dmarcela2022@users.noreply.github.com>
2022-07-21 09:25:00 +0200

    footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
-import './Footer.scss';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon
+} from 'mdb-react-ui-kit';
 
-const Footer = () => {
+export default function App() {
   return (
-    <div>Footer</div>
+    <MDBFooter className='bg-dark text-center text-white'>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </a>
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='github' />
+          </a>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:
+        <a className='text-white' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
   );
 }
 
-export default Footer;
+/*import React from 'react';
+import './Footer.scss';
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="card">
+      <div className="card-title">
+        <div className="card-text">
+          <Link to="/comofunciona"> ¿Como funciona? </Link>
+
+          <Link to="/quienessomos"> ¿Quienes somos? </Link>
+
+          <Link to="/metodosdepago"> Métodos de Pago </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Footer;*/
